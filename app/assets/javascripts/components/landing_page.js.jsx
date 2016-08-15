1345f0f7709ddeeb8b39fb639700f98e098fe741
DeProdigy <alex.hint.ah@gmail.com>
2016-08-14 21:56:47 -0400

    Main Screen with Sass reset

--- app/assets/javascripts/components/landing_page.js.jsx
+++ app/assets/javascripts/components/landing_page.js.jsx
@@ -1,8 +1,8 @@
 var LandingPage = React.createClass({
   render: function() {
     return (
-      <div>
-        <h1>HELLOOOOO WOORODLDLDLDL; I AM REACT</h1>
+      <div className='landing-page-container'>
+        <MainScreen />
       </div>
     );
   }
